742ff5cd94a28cf90b323f67794988a5cfc6a7f3
Andrew <papsujevicandrew@gmail.com>
2023-05-21 14:05:41 +0300

    refactoring with hooks

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -4,54 +4,60 @@ import { Section } from '../Section/Section';
 import { Statiscics } from 'components/Statistics/Statisctics';
 import { Wrapper } from './Feedback.styled';
 
-const { Component } = require('react');
-// refactor here
-class Feedback extends Component {
-  state = {
-    good: 0,
-    bad: 0,
-    neutral: 0,
-  };
-  handleIncrement = name => {
-    this.setState(prevState => ({
-      [name]: prevState[name] + 1,
-    }));
-  };
-  calculateTotal = () => {
-    return this.state.good + this.state.neutral + this.state.bad;
-  };
-  calculatePercentage = () => {
-    return this.calculateTotal() === 0
-      ? `No data`
-      : `${Math.round((this.state.good / this.calculateTotal()) * 100)}%`;
-  };
-  render() {
-    const options = Object.keys(this.state);
-    return (
-      <Wrapper>
-        <Section title="Feedback">
-          <FeedbackOptions
-            options={options}
-            onLeaveFeedback={this.handleIncrement}
-          />
-        </Section>
+const { useState } = require('react');
 
-        <Section title="Statistics">
-          {this.calculateTotal() === 0 ? (
-            <Notification message="Please enter feedback" />
-          ) : (
-            <Statiscics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={this.calculateTotal()}
-              positivePercentage={this.calculatePercentage()}
-            />
-          )}
-        </Section>
-      </Wrapper>
-    );
-  }
-}
+export const Feedback = () => {
+  const [good, setGood] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const options = ['good', 'bad', 'neutral'];
+
+  const handleIncrement = name => {
+    switch (name) {
+      case 'good':
+        setGood(prevGood => prevGood + 1);
+        break;
+      case 'bad':
+        setBad(prevBad => prevBad + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevNeutral => prevNeutral + 1);
+        break;
+      default:
+        console.error(`Something went wrong, try again.`);
+        break;
+    }
+  };
+  const calculateTotal = () => {
+    return good + neutral + bad;
+  };
+  const calculatePercentage = () => {
+    return calculateTotal() === 0
+      ? `No data`
+      : `${Math.round((good / calculateTotal()) * 100)}%`;
+  };
+
+  return (
+    <Wrapper>
+      <Section title="Feedback">
+        <FeedbackOptions options={options} onLeaveFeedback={handleIncrement} />
+      </Section>
+
+      <Section title="Statistics">
+        {calculateTotal() === 0 ? (
+          <Notification message="Please enter feedback" />
+        ) : (
+          <Statiscics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={calculateTotal()}
+            positivePercentage={calculatePercentage()}
+          />
+        )}
+      </Section>
+    </Wrapper>
+  );
+};
 
 export default Feedback;
